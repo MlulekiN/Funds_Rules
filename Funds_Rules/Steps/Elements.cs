8d2f3a23eaf78a9ffe0389df9ae28b6d42744510
Mlulekigit config --global user.email mlu78nkosi@gmail.com <mlu78nkosi@gmail.com>
2022-04-14 15:39:26 +0200

    Add project files.

---
+++ Funds_Rules/Steps/Elements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Funds_Rules.Steps
+{
+    public class Elements
+    {
+        public static By homeToolBar = By.XPath("//div[@sm1-id='homeToolbarMenuBTN']");
+        public static By searchBox = By.XPath("//div[@sm1-id='mainMenuSearchBox']/descendant::input[@placeholder='Search']");
+        public static By viewCompleteMenu = By.XPath("//span[text()='View Complete Menu']");
+        public static By ToolFunction(string getTab) => By.XPath($"//span[text()='{getTab}']"); 
+        public static By addButton = By.XPath("//span[@sm1-id='AddButton']");
+        public static By dropDownList = By.XPath("//span[text()='Configuration ID:']//ancestor::label/following-sibling::div//div[@class='sm1-triggers']//div[contains(@id,'trigger-select') or contains(@id,'trigger-picker') or contains(@id,'trigger-openStatuses') or contains(@id,'trigger-filtermaker') or contains(@class,'sm1textboxactiontriggerdoc')]");
+        public static By configurationID = By.XPath($"//li[text()='629 - KPI']");
+        public static By configIdOkButton = By.XPath("//span[text()='OK']/parent::span[contains(@id,'sm1okbutton')]");
+        public static By loading = By.XPath("//div[contains(@class, 'x-component sm1-mask x-mask x-component-default')]");
+        public static By selectConfigID(string selectConfig) => By.XPath($"//li[text()='{selectConfig}']"); 
+        public static By inputText = By.XPath("//div[@sm1-id='LEVEL_CLIENT']/descendant::div[@class='sm1-triggers']");
+        public static By customerLevelDropList = By.XPath("//span[text()='Customer level:']//ancestor::label/following-sibling::div//div[@class='sm1-triggers']//div[contains(@id,'trigger-select') or contains(@id,'trigger-picker') or contains(@id,'trigger-openStatuses') or contains(@id,'trigger-filtermaker') or contains(@class,'sm1textboxactiontriggerdoc')]");
+        public static By CustomerLevel(string level) =>  By.XPath($"//li[text()='{level}']");
+        public static By startDatePicker = By.XPath("//input[@data-ref='startDtpEl']");
+        public static By monthSelect = By.XPath("//div[contains(@class,'x-datepicker')]/child::div[contains(@sm1-id,'PICKERFIELD-sm1dateperiod')]/descendant::a[@data-qtip='Choose Month']");
+        public static By innerMonthOkButton = By.XPath("//div[contains(@class,'x-monthpicker x-monthpicker')]/descendant::span[not(text()='Cancel') and @class='x-btn-inner x-btn-inner-default-small']/parent::span");
+        public static By specificDate(string SelectMonth) => By.XPath($"//div[@data-ref='monthEl']/child::div/a[text()='{SelectMonth}']/parent::div']");
+        public static By selectDay(string day) => By.XPath($"//div[@class='x-datepicker-date' and text()='{day}']");
+        public static By productDownList = By.XPath("//span[text()='Product Groups:']//ancestor::label/following-sibling::div//div[@class='sm1-triggers']//div[contains(@id,'trigger-select') or contains(@id,'trigger-picker') or contains(@id,'trigger-openStatuses') or contains(@id,'trigger-filtermaker') or contains(@class,'sm1textboxactiontriggerdoc')]");
+    }
+    
+}
---
+++ Funds_Rules/Steps/Promo_Rules.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using TechTalk.SpecFlow;
+
+
+namespace Funds_Rules.Steps
+{
+    [Binding]
+
+    public sealed class Promo_Rules
+    {
+        public string url = "https://to-pdi.salesperformanceplatform.com/xtelsp-automhfx/web/SM1V6/SM1.aspx?DBG=1";
+        // public string url = "https://to-pdi.salesperformanceplatform.com/xtelsp-autom/web/?DBG=1";
+        public static IWebDriver driver;
+        public static WebDriverWait Wait;
+        public static int test;
+
+        [Given(@"The user has logged in the application")]
+        public void GivenTheUserHasLoggedInTheApplication()
+        {
+            test = 2;
+            ChromeOptions options = new ChromeOptions();
+
+            var userProfile = @"%UserProfile%\AppData\Local\Google\Chrome\User Data\Profile 2";
+            options.AddArgument("--profile-directory=Profile 2");
+
+            driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), options, TimeSpan.FromSeconds(180));
+            Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(500)
+            };
+            wait.IgnoreExceptionTypes(typeof(TimeoutException), typeof(NoSuchElementException));
+            wait.Until(ExpectedConditions.ElementIsVisible(Elements.homeToolBar));
+        }
+
+        [When(@"User clicks on toolbar menu")]
+        public void WhenUserClicksOnToolbarMenu()
+        {
+            Console.WriteLine(test);
+            IWebElement homePage = Wait.Until(ExpectedConditions.ElementIsVisible(Elements.homeToolBar));
+            homePage.Click();
+        }
+
+        [When(@"User clicks on view complete menu")]
+        public void WhenUserClicksOnViewCompleteMenu()
+        {
+            IWebElement viewCompleteMenu = driver.FindElement(Elements.searchBox);
+            viewCompleteMenu.Click();
+            viewCompleteMenu.SendKeys("Funds & Rules");
+        }
+
+        [When(@"User clicks on '(.*)'")]
+        public void WhenUserClicksOn(string getTab)
+        {
+            IWebElement toolTab = Wait.Until(ExpectedConditions.ElementIsVisible(Elements.ToolFunction(getTab)));
+            toolTab.Click();
+        }
+
+        [Then(@"Multiple ID's are displayed\.")]
+        public void ThenMultipleIDSAreDisplayed_()
+        {
+            Assert.IsNotEmpty("toolTab");
+        }
+
+        [When(@"User clicks on the add button")]
+        public void WhenUserClicksOnTheAddButton()
+        {
+            IWebElement addButton = Wait.Until(ExpectedConditions.ElementIsVisible(Elements.addButton));
+            addButton.Click();
+        }
+
+        [When(@"Inputs '(.*)'")]
+        public void WhenInputs(string configSelect)
+        {
+            IWebElement selectID = driver.FindElement(Elements.selectConfigID(configSelect));
+            selectID.Click();
+        }
+
+        [When(@"Clicks on the ok button")]
+        public void WhenClicksOnTheOkButton()
+        {
+            IWebElement configOkButton = Wait.Until(ExpectedConditions.ElementIsVisible(Elements.configIdOkButton));
+            configOkButton.Click();
+        }
+
+        [When(@"User waits for the page to load")]
+        public void WhenUserWaitsForThePageToLoad()
+        {
+            Wait.Until(ExpectedConditions.InvisibilityOfElementLocated(Elements.loading));
+        }
+
+        [When(@"User clicks on customer level dropdown list until '(.*)' is displayed")]
+        public void WhenUserClicksOnCustomerLevelDropdownList(string level)
+        {
+            ClickUntilElementIsDisplayed(Elements.customerLevelDropList, Elements.CustomerLevel(level));
+        }
+
+        [When(@"User selects on the customer level '(.*)'")]
+        public void WhenUserSelectsOnTheCustomerLevel(string level)
+        {
+            IWebElement levelSelect = driver.FindElement(Elements.CustomerLevel(level));
+            levelSelect.Click();
+        }
+
+        [When(@"Clicks on the config id dropdown list until '(.*)' is displayed")]
+        public void WhenClicksOnTheTextTab(string configSelect)
+        {
+            ClickUntilElementIsDisplayed(Elements.dropDownList, Elements.selectConfigID(configSelect));
+        }
+
+        public void ClickUntilElementIsDisplayed(By elementToClick, By elementToValidate)
+        {
+            //  IWebElement column = Wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@data-qtip='Close dialog']")));
+            //  column.Click();
+            driver.FindElement(elementToClick).Click();
+            bool isKPIDisplayed = false;
+            while (!isKPIDisplayed)
+            {
+                if (driver.FindElements(elementToValidate).Count > 0)
+                {
+                    if (driver.FindElement(elementToValidate).Displayed)
+                    {
+                        isKPIDisplayed = true;
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                        driver.FindElement(elementToClick).Click();
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    driver.FindElement(elementToClick).Click();
+                }
+            }
+        }
+
+        [When(@"User selects the sell in period (.*)")]
+        public void WhenUserSelectsTheSellInPeriod(string sellInDate)
+        {
+            string date = "21/Apr/2022";
+            string expectdateDay = date.Split("/")[0];
+            string expectdateMonth = date.Split("/")[1];
+            string expectdateYear = date.Split("/")[2];
+
+            IWebElement selectDate = driver.FindElement(Elements.startDatePicker);
+            selectDate.Click();
+            IWebElement selectMonth = driver.FindElement(Elements.monthSelect);
+            selectMonth.Click();
+            driver.FindElement(Elements.specificDate(expectdateMonth)).Click();
+            driver.FindElement(Elements.specificDate(expectdateYear)).Click();
+            driver.FindElement(Elements.innerMonthOkButton).Click();
+            driver.FindElement(Elements.selectDay(expectdateDay)).Click();
+            driver.FindElement(By.XPath("//span[text()='OK']/parent::span")).Click();
+        }
+
+        [When(@"User clicks on product groups drop down list")]
+        public void WhenUserClicksOnProductGroupsDropDownList()
+        {
+            driver.FindElement(Elements.productDownList).Click();
+        }
+
+    }
+}
