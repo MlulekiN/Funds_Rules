b242fb44135d0a17ca6a359431e104d32eed5627
Mlulekigit config --global user.email mlu78nkosi@gmail.com <mlu78nkosi@gmail.com>
2022-04-14 15:39:26 +0200

    Funds and Rules Add project files.

---
+++ Funds_Rules/Features/Promo_Rules.feature
@@ -0,0 +1,24 @@
+﻿Feature: Configuring Funds&Rules and adding a Promo
+
+Scenario: The user needs to configure the funds and rules and add a promo
+	#step 1
+	Given The user has logged in the application
+	When User clicks on toolbar menu
+	And User clicks on view complete menu
+	And User clicks on 'Funds & Rules'
+	Then Multiple ID's are displayed.
+	#step 2
+	When User clicks on the add button
+	#And Clicks on the text tab 
+	And Clicks on the config id dropdown list until '629 - KPI' is displayed
+	And Inputs '629 - KPI'
+	And Clicks on the ok button
+	And User waits for the page to load
+	And User clicks on customer level dropdown list until '5 - Level 5' is displayed
+	And User selects on the customer level '5 - Level 5'
+	And User selects the sell in period "01/04/2022"
+	And sell out date "30/04/2022"
+	And User clicks on product groups drop down list 
+	And User selects the product and clicks the ok button
+	And User clicks the save button
+		 
---
+++ Funds_Rules/Steps/Elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Funds_Rules.Steps
+{
+    public class Elements
+    {
+        public static By homeToolBar = By.XPath("//div[@sm1-id='homeToolbarMenuBTN']");
+        public static By searchBox = By.XPath("//div[@sm1-id='mainMenuSearchBox']/descendant::input[@placeholder='Search']");
+        public static By viewCompleteMenu = By.XPath("//span[text()='View Complete Menu']");
+        public static By ToolFunction(string getTab) => By.XPath($"//span[text()='{getTab}']"); 
+        public static By addButton = By.XPath("//span[@sm1-id='AddButton']");
+        public static By dropDownList = By.XPath("//span[text()='Configuration ID:']//ancestor::label/following-sibling::div//div[@class='sm1-triggers']//div[contains(@id,'trigger-select') or contains(@id,'trigger-picker') or contains(@id,'trigger-openStatuses') or contains(@id,'trigger-filtermaker') or contains(@class,'sm1textboxactiontriggerdoc')]");
+        public static By configurationID = By.XPath($"//li[text()='629 - KPI']");
+        public static By configIdOkButton = By.XPath("//span[text()='OK']/parent::span[contains(@id,'sm1okbutton')]");
+        public static By loading = By.XPath("//div[contains(@class, 'x-component sm1-mask x-mask x-component-default')]");
+        public static By selectConfigID(string selectConfig) => By.XPath($"//li[text()='{selectConfig}']"); 
+        public static By inputText = By.XPath("//div[@sm1-id='LEVEL_CLIENT']/descendant::div[@class='sm1-triggers']");
+        public static By customerLevelDropList = By.XPath("//span[text()='Customer level:']//ancestor::label/following-sibling::div//div[@class='sm1-triggers']//div[contains(@id,'trigger-select') or contains(@id,'trigger-picker') or contains(@id,'trigger-openStatuses') or contains(@id,'trigger-filtermaker') or contains(@class,'sm1textboxactiontriggerdoc')]");
+        public static By CustomerLevel(string level) =>  By.XPath($"//li[text()='{level}']");
+        public static By startDatePicker = By.XPath("//input[@data-ref='startDtpEl']");
+        public static By endDatePicker = By.XPath("//input[@data-ref='endDtpEl']");
+        public static By monthSelect = By.XPath("//div[contains(@class,'x-datepicker')]/child::div[contains(@sm1-id,'PICKERFIELD-sm1dateperiod')]/descendant::a[@data-qtip='Choose Month']");
+        public static By innerMonthOkButton = By.XPath("//div[contains(@class,'x-monthpicker x-monthpicker')]/descendant::span[not(text()='Cancel') and @class='x-btn-inner x-btn-inner-default-small']/parent::span");
+        public static By SpecificDateMonth(string SelectMonth) => By.XPath($"//div[@data-ref='monthEl']/child::div/a[text()='{SelectMonth}']/parent::div");
+        public static By specificDateYear(string SelectYear) => By.XPath($"//div[@data-ref='yearEl']/child::div/a[text()='{SelectYear}']/parent::div");
+        public static By selectDay(string day) => By.XPath($"//div[@class='x-datepicker-date' and text()='{day}']");
+        public static By productDownList = By.XPath("//span[text()='Product Groups:']//ancestor::label/following-sibling::div//div[@class='sm1-triggers']//div[contains(@id,'trigger-select') or contains(@id,'trigger-picker') or contains(@id,'trigger-openStatuses') or contains(@id,'trigger-filtermaker') or contains(@class,'sm1textboxactiontriggerdoc')]");
+        public static By productSelect = By.XPath("//div[text()='ITALIAN WINE']/parent::td/preceding-sibling::td[contains(@class,'checkcolumn')]");
+        public static By productOkButton = By.XPath("//div[text()='Product level']/ancestor::div[contains(@class,'x-horizontal')]/following-sibling::div/descendant::span[text()='OK']/parent::span");
+        public static By saveButton = By.XPath("//a[@sm1-id='TLBSAVEBUTTON']/descendant::span[@data-ref='btnIconEl']");
+    }
+    
+}
